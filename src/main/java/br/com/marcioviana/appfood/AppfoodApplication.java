24067397ab5b5ca397a392e20f74707e5bb11bdd
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-02-23 09:31:05 -0300

    First commit

---
+++ src/main/java/br/com/marcioviana/appfood/AppfoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.appfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcioviana/appfood/AppfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcioviana.appfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
